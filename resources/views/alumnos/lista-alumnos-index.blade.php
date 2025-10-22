8634537655f599e8b39c61331d410caab9579c07
Daninvg <cristian.navarrete6778@alumnos.udg.mx>
2025-10-21 22:30:38 -0600

    Se agrega un boton al index para poder eliminar alumnos

--- resources/views/alumnos/lista-alumnos-index.blade.php
+++ resources/views/alumnos/lista-alumnos-index.blade.php
@@ -10,7 +10,7 @@
 <body>
     <h1>Vista para alumnos</h1>
     <ul>
-        <li> <a href="{{ route('alumno.create') }} "> Crear un nuevo alumno </a> 
+        <li> <a href="{{ route('alumno.create') }} "> Crear un nuevo alumno </a>
         </li>
     </ul>
     <table border="1">
@@ -24,20 +24,28 @@
                 <th>Sexo</th>
                 <th>Carrera</th>
                 <th>Editar</th>
+                <th>Eliminar</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($alumnos as $alumno)
-                <tr>
-                    <td> {{ $alumno->id }} </td>
-                    <td> <a href="{{ route('alumno.show', $alumno->id) }}"> {{ $alumno->nombre }} </a></td>
-                    <td> {{ $alumno->codigo }} </td>
-                    <td> {{ $alumno->correo }} </td>
-                    <td> {{ $alumno->fecha_nacimiento }} </td>
-                    <td> {{ $alumno->sexo }} </td>
-                    <td> {{ $alumno->carrera }} </td>
-                    <td> <a href="{{ route('alumno.edit', $alumno->id) }}">Editar</a></td>
-                </tr>
+            <tr>
+                <td> {{ $alumno->id }} </td>
+                <td> <a href="{{ route('alumno.show', $alumno->id) }}"> {{ $alumno->nombre }} </a></td>
+                <td> {{ $alumno->codigo }} </td>
+                <td> {{ $alumno->correo }} </td>
+                <td> {{ $alumno->fecha_nacimiento }} </td>
+                <td> {{ $alumno->sexo }} </td>
+                <td> {{ $alumno->carrera }} </td>
+                <td> <a href="{{ route('alumno.edit', $alumno->id) }}">Editar</a></td>
+                <td>
+                    <form action="{{ route('alumno.destroy', $alumno->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit"> Eliminar registro </button>
+                    </form>
+                </td>
+            </tr>
             @endforeach
         </tbody>
     </table>
