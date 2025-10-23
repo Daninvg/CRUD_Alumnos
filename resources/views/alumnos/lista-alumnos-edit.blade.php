2fcadfd16ed77833ed400cbaea9a9e103da5c9ad
Daninvg <cristian.navarrete6778@alumnos.udg.mx>
2025-10-22 21:38:49 -0600

    Se agrega el component a la vista editar

--- resources/views/alumnos/lista-alumnos-edit.blade.php
+++ resources/views/alumnos/lista-alumnos-edit.blade.php
@@ -5,13 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar alumno</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" 
+    rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 </head>
 
 <body>
 
-    <h1>Edicion de alumnos</h1>
+
+<x-encabezado>
+    Editar alumno: {{$alumno->nombre}}
+</x-encabezado>
     
-    @include('formulario-error')
+<x-Formulario-error/>
     <form action="{{ route('alumno.update', $alumno->id) }}" method="POST">
         @csrf
         @method('PATCH')
